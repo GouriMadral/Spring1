3cb02c6e7e8e61e691e5c69ec67673e604706a02
GouriMadral <madralgouri0503@gmail.com>
2025-05-03 13:43:38 +0530

    Add files via upload

---
+++ Service/OrdersService.java
@@ -0,0 +1,13 @@
+package com.food.service;
+import java.util.List;
+import com.food.entity.Orders;
+
+public interface OrdersService {
+	
+	Orders createOrders(Orders orders);	//Creates a new orders record in the database.
+	Orders getOrders(String orderID);  //Retrieves a list of all orders records from the database.
+	List<Orders> getAllOrders();  // Fetches a single orders by its unique ID.
+	Orders updateOrders(String oId, Orders order);  //Updates the details of an existing orders identified by ordersID.
+	String deleteOrders(String id); //Deletes an orders record identified by ordersID.
+
+}
