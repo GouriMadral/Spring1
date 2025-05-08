0f20ff3159952f98661f078cc869e2fdf993af49
GouriMadral <madralgouri0503@gmail.com>
2025-05-08 14:25:40 +0530

    Add files via upload

---
+++ Food_Ordering_Sytem/Service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.food.service;
+import java.util.List;
+import com.food.entity.Customer;
+
+public interface CustomerService {
+	
+	Customer createCustomer(Customer customer);	//Creates a new customer record in the database.
+	List<Customer> getAllCustomer(); //Retrieves a list of all customer records from the database.
+	Customer getCustomer(String customerID); // Fetches a single customer by its unique ID.
+	Customer updatecustomer(String customerID,Customer updatedCustomer); //Updates the details of an existing customer identified by customerID.
+	String deleteCustomer(String customerID); //Deletes an customer record identified by customerID.
+}
